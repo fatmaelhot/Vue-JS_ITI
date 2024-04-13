4381d3238c72896748305c64ed14b518c36e6c5d
fatma nasser <128849238+fatmaelhot@users.noreply.github.com>
2024-04-13 12:01:56 +0200

    lab2

---
+++ Lab2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e33;
+  margin-top: 60px;
+}
+</style>
